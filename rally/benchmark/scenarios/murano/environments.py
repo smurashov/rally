45ff05045cfb86cea3cd3a3f90685834fe3a4993
Sergey Murashov <smurashov@mirantis.com>
2014-08-27 14:03:36 +0400

    Add deployment tests

--- rally/benchmark/scenarios/murano/environments.py
+++ rally/benchmark/scenarios/murano/environments.py
@@ -1,7 +1,7 @@
 from rally.benchmark.scenarios.murano import utils
 from rally.openstack.common import log as logging
 from rally.benchmark.scenarios import base
-
+from rally import exceptions as rally_exceptions
 
 LOG = logging.getLogger(__name__)
 
@@ -11,9 +11,29 @@ class MuranoEnvironments(utils.MuranoScenario):
     def __init__(self, *args, **kwargs):
         super(MuranoEnvironments, self).__init__(*args, **kwargs)
 
-    @base.scenario(context={"cleanup": ["murano"]})
+    @base.scenario()
     def get_list_environments(self):
-        environment = self._create_environment(self._generate_random_name())
         self._list_environments()
+
+
+    @base.scenario(context={"cleanup": ["murano"]})
+    def create_environment_create_session_delete_environment(self):
+        environment = self._create_environment(self._generate_random_name())
+
         self._create_session(environment.id)
+        self._delete_environment(environment.id)
+
+    @base.scenario(context={"cleanup": ["murano"]})
+    def deploy_environment(self, image_name, flavor_name):
+        environment = self._create_environment(self._generate_random_name())
+        session = self._create_session(environment.id)
+
+        self._add_app(environment.id, session.id, image_name, flavor_name)
+        self._deploy_environment(environment.id, session.id)
+        self._wait_finish_of_deploy(environment)
+
+        deployment = self._get_deployments_list(environment.id)[-1]
+        if deployment.state != 'success':
+            raise rally_exceptions.TestException("Deployment failed")
+
         self._delete_environment(environment.id)
--- rally/benchmark/scenarios/murano/utils.py
+++ rally/benchmark/scenarios/murano/utils.py
@@ -1,7 +1,11 @@
+import random
+import uuid
+
 from oslo.config import cfg
 
 from rally.benchmark.scenarios import base
 from rally.benchmark.scenarios import utils as scenario_utils
+from rally.benchmark import utils as bench_utils
 
 
 class MuranoScenario(base.Scenario):
@@ -27,3 +31,90 @@ class MuranoScenario(base.Scenario):
     def _create_session(self, environment_id):
 
         return self.clients('murano').sessions.configure(environment_id)
+
+    @scenario_utils.atomic_action_timer('murano.create_service')
+    def _add_app(self, environment_id, session_id, image_name, flavor_name):
+
+        app = self._get_random_app(image_name, flavor_name)
+
+        return self.clients('murano').services.post(environment_id, path='/',
+                                                    data=app,
+                                                    session_id=session_id)
+
+    @scenario_utils.atomic_action_timer('murano.deploy_environment')
+    def _deploy_environment(self, environment_id, session_id):
+
+        return self.clients('murano').sessions.deploy(environment_id,
+                                                      session_id)
+
+    def _get_random_app(self, image_name, flavor_name):
+        k = random.randint(0, 100)
+
+        if k <= 33:
+            return {
+                "instance": {
+                    "?": {
+                        "type": "io.murano.resources.LinuxMuranoInstance",
+                        "id": str(uuid.uuid4())
+                    },
+                    "flavor": flavor_name,
+                    "image": image_name,
+                    "name": "instance{0}".format(uuid.uuid4().hex[:5])
+                },
+                "name": "app{0}".format(uuid.uuid4().hex[:5]),
+                "?": {
+                    "type": "io.murano.apps.linux.Telnet",
+                    "id": str(uuid.uuid4())
+                }
+            }
+
+        elif k <= 66:
+            return {
+                "instance": {
+                    "?": {
+                        "type": "io.murano.resources.LinuxMuranoInstance",
+                        "id": str(uuid.uuid4())
+                    },
+                    "flavor": flavor_name,
+                    "image": image_name,
+                    "name": "instance{0}".format(uuid.uuid4().hex[:5])
+                },
+                "name": "app{0}".format(uuid.uuid4().hex[:5]),
+                "?": {
+                    "type": "io.murano.apps.apache.Apache",
+                    "id": str(uuid.uuid4())
+                }
+            }
+
+        else:
+            return {
+                "instance": {
+                    "?": {
+                        "type": "io.murano.resources.LinuxMuranoInstance",
+                        "id": str(uuid.uuid4())
+                    },
+                    "flavor": flavor_name,
+                    "image": image_name,
+                    "name": "instance{0}".format(uuid.uuid4().hex[:5])
+                },
+                "name": "app{0}".format(uuid.uuid4().hex[:5]),
+                "?": {
+                    "type": "io.murano.apps.PostgreSql",
+                    "id": str(uuid.uuid4())
+                }
+            }
+
+    @scenario_utils.atomic_action_timer('murano.wait_finish_of_deploy')
+    def _wait_finish_of_deploy(self, environment):
+
+        bench_utils.wait_for(
+            environment, is_ready=bench_utils.resource_is("READY"),
+            update_resource=bench_utils.get_from_manager(),
+            timeout=1200.0,
+            check_interval=5.0
+        )
+
+    @scenario_utils.atomic_action_timer('murano.get_deployments_list')
+    def _get_deployments_list(self, environment_id):
+
+        return self.clients('murano').deployments.list(environment_id)
